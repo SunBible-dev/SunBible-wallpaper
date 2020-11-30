e51fc4bd9cdd20ec816f55290938819ec242c261
TheSunShining <thesunshiningmin@gmail.com>
2020-11-30 15:03:53 -0700

    start!

---
+++ main.js
@@ -0,0 +1,30 @@
+// The "BrowserWindow" class is the primary way to create user interfaces
+// in Electron. A BrowserWindow is, like the name suggests, a window.
+//
+// For more info, see:
+// https://electronjs.org/docs/api/browser-window
+
+const { app, BrowserWindow } = require('electron')
+
+
+
+app.whenReady().then(() => {
+
+    const mainWindow = new BrowserWindow({
+        resizable: false,
+        alwaysOnTop: true,
+        maximizable: false,
+        minimizable: false,
+        width: 800,
+        height: 500,
+        webPreferences: {
+            nodeIntegration: true,
+            webviewTag: true
+        }
+    })
+
+    // Load our index.html
+    mainWindow.loadFile('index.html')
+    mainWindow.removeMenu()
+
+})
